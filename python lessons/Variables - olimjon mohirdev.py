2ee0f41c9c5a94d67f72c6fe8a5178d99309967d
Mister Olimjon <olimjon260303@gmail.com>
2025-11-11 11:03:01 +0900

    new file
    new

---
+++ python lessons/Variables - olimjon mohirdev.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  3 22:07:56 2025
+
+@author: tojiboevolimjon
+"""
+
+#!!!!Variables (o'zgaruvchilar)
+
+name='olimjon'
+age =23
+print(name)
+print(age)
+
+
+a=6
+b=7
+c=(6+7)**2
+print(c)
+
+
+
+#Practice(amaliyot)
+#Assign the text "Hello World!" to a new variable and display it in the console using print().
+text='hello world'
+print(text)
+#Create a variable named xabar, assign it some text, and display it in the console. Then, give the variable a new value and display it again.
+
+xabar='welcome'
+print(xabar)
+xabar=23+2
+print(xabar)
+
+radius=5
+pi=3.14159
+aynalanani_yuzi = pi*radius**2
+print("aylana radiusi",radius ,"ga teng aylananing yuzi=",aynalanani_yuzi)
